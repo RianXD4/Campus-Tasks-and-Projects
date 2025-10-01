b3c72eb8b3cc6477fb3d1407ba5388d3099f50aa
naufalazmiaqil <incoldtown@gmail.com>
2025-10-01 13:35:58 +0700

    commit

--- FinalProject01.java
+++ FinalProject01.java
@@ -2,5 +2,6 @@ public class FinalProject01 {
 
     public static void main(String[] args) {
         System.out.println("Hi, ini kode baris Adrian_01,YIPPIIEEE");
+        System.out.println("halo aku nopal");
     }
 }
